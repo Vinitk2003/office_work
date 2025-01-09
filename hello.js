cffcb19b37b08db2fbf85b9e3ae13b6e43f40656
Vinitk2003 <vinit.glsbca20@gmail.com>
2025-01-09 18:28:31 +0530

    first vcommit

---
+++ hello.js
@@ -0,0 +1,96 @@
+         ////reference datatypes
+
+     /// obj
+// let person = {
+//     firstName : "Elon",
+//     lastName : 'Musk',
+//     age : 30
+// };
+
+// console.log(person);
+// console.log(typeof person)
+
+     ///array
+
+// let num = [1,2,3,4,5];
+
+// console.log(num);
+// console.log(typeof num);
+
+     ///Function
+
+// function msg(){
+//     console.log("Hello, Greate work");
+// }
+
+// console.log(typeof msg);
+
+        ////Data types
+
+// let x;
+// console.log(x);
+// console.log(typeof x);
+
+// x = "completed";
+// console.log(x);
+// console.log(typeof x);
+
+// x = 100;
+
+// console.log(x);
+// console.log(typeof x);
+
+// x = 100;
+
+// console.log(x);
+// console.log(typeof x);
+
+        ////Operators
+
+     /// Arithmetic 
+
+// // Addition
+// let sum = 5 + 3;
+// console.log(sum);
+// // Subtraction
+// let x = 5 - 3;
+// console.log(x);
+// // Multiplication
+// let y = 5 * 3;
+// console.log(y);
+// // Division
+// let z = 15 / 3;
+// console.log(z);
+// // Modulus
+// let b = 95 % 3;
+// console.log(b);
+// //Exponention
+// let c = 5 ** 3; //5*5*5
+// console.log(c);
+
+
+            /////Assignment Operator
+  // Add
+let x = 15;
+
+x += 3;
+console.log(x);
+//Substrct
+x -= 5;
+console.log(x);
+//Multiply
+x *= 3;
+console.log(x);
+//Divide
+x /= 3;
+console.log(x);
+
+
+
+
+
+
+
+
+
+
