1ad0d686979dfa71edb84f9798ce0837fde393c6
Vinitk2003 <vinit.glsbca20@gmail.com>
2025-01-17 14:51:57 +0530

    python practice

---
+++ programme/seven.py
@@ -0,0 +1,13 @@
+#slicing
+# str = "apna college"
+# print(str[-5:-1])  
+
+
+#string functuins
+str = "i am studying py from online "
+print(str.endswith("vbgf"))
+print(str.capitalize())
+print(str.count("a"))
+print(str.find("py"))
+print(str.replace("py", "java"))
+print(str.split(" "))
---
+++ programme/six.py
@@ -0,0 +1,11 @@
+  ## string
+# #concatenation
+# str1 = "hello"
+# print(len(str1))
+# str2 = "world"
+# print(str1 + " " + str2)  # Output: hello world
+
+#indexing
+str = "apna college"
+ch = str[4]
+print(ch)  # Output: a
