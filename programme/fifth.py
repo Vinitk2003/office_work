236320a1ab40525d3672ade1031b2914c7b938b3
Vinitk2003 <vinit.glsbca20@gmail.com>
2025-01-17 14:07:14 +0530

    python practice

---
+++ programme/fifth.py
@@ -0,0 +1,25 @@
+# # first = int(input("enter first num:"))
+# # second = int(input("enter second num:"))
+
+# # print("sum", first + second)
+
+
+# side = float(input("enter square side:"))
+
+# print("area=" , side**2)
+
+#practice 3 WAP to input 2 floating point num and print their avg
+# a = float(input("enter first :"))
+# b = float(input("enter first :"))
+
+# print("avg", (a+b)/2)
+
+# practice 4 WAP TO INPUT 2 int nums a and b 
+#print True if a is greater than or eqal to b if not print false
+
+a = int(input("enter first"))
+b = int(input("enter second"))
+
+print(a >= b)
+
+
---
+++ programme/fourth.py
@@ -0,0 +1,22 @@
+#type conversion is doing automatically
+a = 2
+b = 4.25
+sum = a + b # 2 + 4.25 = 6.25
+print(sum)
+
+#type casting is doing manually
+a, b = 1, "2"
+c = int(b)
+sum = a + c
+print(sum)
+
+#also use this
+a = int("23")
+b = 33.4
+
+mul = a * b
+print(mul)
+print(type(a))
+
+
+
