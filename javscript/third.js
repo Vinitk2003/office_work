88b5cc6f4743f7175c9acd0060ae9a8e26a1292d
Vinitk2003 <vinit.glsbca20@gmail.com>
2025-01-21 12:29:09 +0530

    javascript

--- javscript/third.js
+++ javscript/third.js
@@ -1,7 +1,7 @@
-/*operators and conditions
+/*operators 
 for comments we use // or */
 
-// Aeithmetic
+// Arithmetic
 // let a = 5;
 // let b = 3;
 // console.log("a = ", a, "& b =", b); 
@@ -23,13 +23,39 @@ for comments we use // or */
 // console.log("b =", b);
 
 //Assignment operator
-let a = 5;
-let b = 3;
+// let a = 5;
+// let b = 3;
 
-a **= 4
-console.log("a =", b +=4);
-console.log("a =", b -=1);
-console.log("a =", a *=4);
-console.log("a =", a /=4);
-console.log("a =", a %=4);
-console.log("a =", a); // not supported in js
+// a **= 4
+// console.log("a =", b +=4);
+// console.log("a =", b -=1);
+// console.log("a =", a *=4);
+// console.log("a =", a /=4);
+// console.log("a =", a %=4);
+// console.log("a =", a); // not supported in js
+
+//Comparison operator
+// let a = 5;
+// let b = 3;
+// let c = 5;
+// console.log("a > b", a > b);
+// console.log("a < b", a < b);
+// console.log("a == b", a == b);
+// console.log("a != b", a != b);
+// console.log("a === b", a === b);
+// console.log("a !== b", a !== b);
+// console.log("a > b > c", a > b > c); // not supported in
+
+//logical operator
+let a = 5;
+let b = 5;
+let c = 6;
+let d = 7
+//logical AND &&
+console.log("a && b", a >= b && a === b); // true when a both con is true
+
+//logicsl or ||
+console.log("c || d", c >= d || c === d);// false when a both con is false
+
+//logical NOT !
+console.log("!(6 < 7)", !(c < d)); // if con is false then print true and if condition is true so print false do opposite
